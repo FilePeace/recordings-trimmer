c45da63eec067baa16a8d842ea7aa3d50f80e955
Daniella Mesquita <mesquitadaniella@proton.me>
2022-11-13 03:24:43 -0300

    Complement previous commit

--- core.sh
+++ core.sh
@@ -1,3 +1,4 @@
+if [ "$1" = "to_implement" ];then
 sudo apt install sox
 # https://stackoverflow.com/a/8954707/5623661
 sox --info -d 
@@ -7,9 +8,20 @@ sox --info -d
 ls -la --time-style=full-iso Gravando_100.wav
 ./timecalc 14:50:40.00 - 00:15:11.70
 # https://github.com/octacian/timecalc/issues/1
+fi
 
-# 3:09:20 to 3:10:03
-#ffmpeg -ss 3:09:20 -i Gravando_102.wav -c copy -t 3:10:03 Gravando_102_trimmed.wav
-ffmpeg -ss 3:09:20 -i 'Gravando_102.wav' -to 00:00:43 -c copy Gravando_102_trimmed.wav
-# https://stackoverflow.com/questions/46508055/using-ffmpeg-to-cut-audio-from-to-position
+
+if [ "$1" = "trim" ];then
+   if [ "$2" != "" ];then
+         if [ "$3" != "" ];then
+            if [ "$4" != "" ];then
+               ffmpeg -ss $3 -i $2 -to 00:00:43 -c copy $2_trimmed_.wav
 # https://stackoverflow.com/a/58253068/5623661, https://stackoverflow.com/a/46509213/5623661
+            fi
+         fi
+   fi
+fi
+
+trim Gravando_102.wav 3:09:20 00:00:43
+echo "3:09:20" | tr ':' '-'
+# | tr -d ":"
